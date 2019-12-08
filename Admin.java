1a5620c9cf00f61a59da905f3fa1453265af594c
wei225 <57899485+wei225@users.noreply.github.com>
2019-12-08 22:00:50 +0800

    Add files via upload

---
+++ Admin.java
@@ -0,0 +1,33 @@
+package top.hiai.model;
+/**
+ * ”√ªßModel¿‡
+ * @author www.hiai.top
+ *
+ */
+public class Admin {
+	private int adminId;
+	private String password;
+	
+	public Admin() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Admin(int adminId, String password) {
+		super();
+		this.adminId = adminId;
+		this.password = password;
+	}
+	public int getAdminId() {
+		return adminId;
+	}
+	public void setAdminId(int adminId) {
+		this.adminId = adminId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
---
+++ StudentDao.java
@@ -0,0 +1,49 @@
+package top.hiai.dao;
+/**
+ * @author www.hiai.top
+ */
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
+
+import top.hiai.model.Course;
+import top.hiai.model.Sinfo;
+import top.hiai.model.Student;
+import top.hiai.util.StringUtil;
+
+public class StudentDao {
+	public ResultSet StudentList(Connection con,Sinfo sinfo) throws SQLException{
+		StringBuffer sb=new StringBuffer("select * from t_sinfo ");
+if(sinfo.getSno()!=-1){
+	sb.append(" and Sno="+sinfo.getSno());
+}
+if(StringUtil.isNotEmpty(sinfo.getSname())){
+	sb.append(" and Sname like '%"+sinfo.getSname()+"%'");
+}
+PreparedStatement pstmt=con.prepareStatement(sb.toString().replaceFirst("and", "where"));
+return pstmt.executeQuery();
+		}
+	
+public ResultSet PasswordList(Connection con,Student student)throws SQLException{
+	StringBuffer sb=new StringBuffer("select * from t_slogon ");
+	if(student.getSno()!=-1){
+		sb.append("where Sno="+student.getSno());
+		}
+		PreparedStatement pstmt=con.prepareStatement(sb.toString());
+		return pstmt.executeQuery();
+	
+}
+public int PasswordModify(Connection con,Student student)throws Exception{
+	String sql="update t_slogon set Spassword=? where Sno=? ";
+	PreparedStatement pstmt=con.prepareStatement(sql);
+	pstmt.setString(1, student.getSpassword());
+	pstmt.setInt(2, student.getSno());
+	return pstmt.executeUpdate();
+	
+} 
+
+}
+
